60309d3be3a104b627fcfc0be4df4a7573beef4d
moonye@live.fr <moonye@live.fr>
2024-12-03 15:11:37 +0100

    ADD : fichier PHP base

---
+++ algo.php
@@ -0,0 +1,39 @@
+<?php
+// exo 1
+$name = "Elodie <br>";
+
+// exo 2
+$lastname = "FOUGEROUSE <br>";
+$firstname = "Elodie <br>";
+$age = "29 ans <br>";
+
+// exo 3
+$km =1;
+$km =3;
+$km =125;
+"<br>"
+;
+
+// exo 4
+$text = "Variable string <br>";
+$int = 3 ; 
+$float = 5.5 ;
+
+// exo 5
+$int5 = null ; 
+$int5 = 10 ; 
+
+// exo 6
+$exo6name = "Elodie";
+
+// exo 7 
+$exo7lastname = "Fougerouse";
+$exo7firstname = "Elodie";
+$exo7agename = 29;
+
+
+// exo 8
+$exo8var1 = 3 + 4;
+$exo8var2 = 5*20;
+$exo8var3 = 45/5;
+
