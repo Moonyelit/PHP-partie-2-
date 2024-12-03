0dadfa7584b67b5145ef96c79d0a5ed1b09127c2
moonye@live.fr <moonye@live.fr>
2024-12-03 15:58:06 +0100

    Exo fini

--- algo.php
+++ algo.php
@@ -1,39 +1,109 @@
 <?php
 // exo 1
-$name = "Elodie <br>";
+$exo1age = 29;
+
+if ($exo1age >= 18) {
+    echo "<p> Vous êtes majeur <br> </p>";
+} else {
+    echo "<p> Vous êtes mineur <br> </p>";
+}
 
 // exo 2
-$lastname = "FOUGEROUSE <br>";
-$firstname = "Elodie <br>";
-$age = "29 ans <br>";
+
+$isEasy = true;
+
+if ($isEasy) {
+    echo "<p>C'est facile <br></p>";
+} else {
+    echo "<p>C'est  difficile  <br></p>";
+}
+
+echo $isEasy ? "C'est facile <br>" : "C'est  difficile  <br>";
 
 // exo 3
-$km =1;
-$km =3;
-$km =125;
-"<br>"
-;
+$gender = "Femme";
+$age = 29;
+
+if ($gender === "Femme" && $age >= 18) {
+    echo "<p>Vous êtes une femme et vous êtes majeure.</p>";
+} elseif ($gender === "Femme" && $age < 18) {
+    echo "<p>Vous êtes une femme et vous êtes mineure.</p>";
+} elseif ($gender === "Homme" && $age >= 18) {
+    echo "<p>Vous êtes un homme et vous êtes majeur.</p>";
+} elseif ($gender === "Homme" && $age < 18) {
+    echo "<p>Vous êtes un homme et vous êtes mineur.</p>";
+} else {
+    echo "<p>Genre ou âge non reconnu.</p>";
+}
+
 
 // exo 4
-$text = "Variable string <br>";
-$int = 3 ; 
-$float = 5.5 ;
+$exo4magnitude = 4;
+
+switch ($exo4magnitude) {
+    case 1:
+        echo "Micro-séisme impossible à ressentir.<br>";
+        break;
+    case 2:
+        echo "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.<br>";
+        break;
+    case 3:
+        echo "	Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.<br>";
+        break;
+    case 4:
+        echo "Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.<br>";
+        break;
+    case 5:
+        echo "Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.<br>";
+        break;
+        case 6:
+        echo "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.<br>";
+        break;
+        case 7:
+        echo "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.<br>";
+        break;
+    case 8:
+        echo "	Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.<br>";
+        break;
+    case 9:
+        echo "	Séisme capable de tout détruire sur une très vaste zone.<br>";
+}
 
 // exo 5
-$int5 = null ; 
-$int5 = 10 ; 
+$exo5gender = "Femme";
+
+if ($exo5gender != "Homme") {
+    echo "<p>C'est une développeuse !!</p>";
+} else {
+    echo "<p>C'est un developpeur !!</p>";
+}
 
 // exo 6
-$exo6name = "Elodie";
+$exo6age = 29;
+
+if ($exo6age >= 18) {
+    echo "<p>Tu es majeur</p>";
+} else {
+    echo "<p>Tu es mineur</p>";
+}
 
 // exo 7 
-$exo7lastname = "Fougerouse";
-$exo7firstname = "Elodie";
-$exo7agename = 29;
+
+$exo7boolean = true ;
+
+if ($exo7boolean == false ) {
+    echo "<p>Ce n'est pas bon</p>";
+} else {
+    echo "<p>C'est ok</p>";
+}
 
 
 // exo 8
-$exo8var1 = 3 + 4;
-$exo8var2 = 5*20;
-$exo8var3 = 45/5;
+$isOk = true;
+
+if ($isOk) {
+    echo "C'est OK";
+} else {
+    echo "Ce n'est pas bon";
+}
 
--- index.php
+++ index.php
@@ -11,44 +11,6 @@ include_once './algo.php';
 </head>
 <body>
   
-    <?php
-// exo 1
-echo $name;
-  
-  
-// exo 2
-echo $lastname;
-echo $firstname;
-echo $age;
-
-//   exo 3
-  echo $km . "<br>";
-
-  
-//   exo 4
-echo $text;
-echo $int . "<br>";
-echo $float . "<br>";
-
-// exo 5
-echo $int5 . "<br>";
-?>
-
-<!-- exo 6 -->
-<p>Bonjour <?php echo$exo6name ?> ,comment vas tu ? </p>
-
-
-<!-- exo  7 -->
-<p>Bonjour <?php echo $exo7lastname . " " . $exo7firstname; ?>, tu as <?php echo $exo7agename; ?> ans.</p>
-
-<!-- exo 8 -->
-
-<?php
-echo $exo8var1 . "<br>";
-echo $exo8var2 . "<br>";
-echo $exo8var3 . "<br>" ;
-
-?>
 
 </body>
 </html>
